ca98bb785a0ed40323d6a6a46b3b9b6a740011db
GD <GD@DESKTOP-KJ5655N>
2017-11-21 09:36:59 +0900

    테스팅

--- src/test/java/kr/gudi/teatime/TestCase.java
+++ src/test/java/kr/gudi/teatime/TestCase.java
@@ -1,34 +1,32 @@
 package kr.gudi.teatime;
 
 import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
-import java.security.Principal;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Locale;
 import java.util.Map;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletInputStream;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import org.junit.Before;
-import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultHandler;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import org.junit.Before;
+
 
 import kr.gudi.teatime.controller.MainController;
 
@@ -39,547 +37,56 @@ public class TestCase {
 	
 	
 	
-	ModelAndView mav = new ModelAndView();
-	HttpServletRequest req = new HttpServletRequest() {
-		
-		@Override
-		public void setCharacterEncoding(String env) throws UnsupportedEncodingException {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public void setAttribute(String name, Object o) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public void removeAttribute(String name) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public boolean isSecure() {
-			// TODO Auto-generated method stub
-			return false;
-		}
-		
-		@Override
-		public int getServerPort() {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-		
-		@Override
-		public String getServerName() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public String getScheme() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public RequestDispatcher getRequestDispatcher(String path) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public int getRemotePort() {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-		
-		@Override
-		public String getRemoteHost() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public String getRemoteAddr() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public String getRealPath(String path) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public BufferedReader getReader() throws IOException {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public String getProtocol() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public String[] getParameterValues(String name) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public Enumeration getParameterNames() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public Map getParameterMap() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public String getParameter(String name) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public Enumeration getLocales() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public Locale getLocale() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public int getLocalPort() {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-		
-		@Override
-		public String getLocalName() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public String getLocalAddr() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public ServletInputStream getInputStream() throws IOException {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public String getContentType() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public int getContentLength() {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-		
-		@Override
-		public String getCharacterEncoding() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public Enumeration getAttributeNames() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public Object getAttribute(String name) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public boolean isUserInRole(String role) {
-			// TODO Auto-generated method stub
-			return false;
-		}
-		
-		@Override
-		public boolean isRequestedSessionIdValid() {
-			// TODO Auto-generated method stub
-			return false;
-		}
-		
-		@Override
-		public boolean isRequestedSessionIdFromUrl() {
-			// TODO Auto-generated method stub
-			return false;
-		}
-		
-		@Override
-		public boolean isRequestedSessionIdFromURL() {
-			// TODO Auto-generated method stub
-			return false;
-		}
-		
-		@Override
-		public boolean isRequestedSessionIdFromCookie() {
-			// TODO Auto-generated method stub
-			return false;
-		}
-		
-		@Override
-		public Principal getUserPrincipal() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public HttpSession getSession(boolean create) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public HttpSession getSession() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public String getServletPath() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public String getRequestedSessionId() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public StringBuffer getRequestURL() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public String getRequestURI() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public String getRemoteUser() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public String getQueryString() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public String getPathTranslated() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public String getPathInfo() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public String getMethod() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public int getIntHeader(String name) {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-		
-		@Override
-		public Enumeration getHeaders(String name) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public Enumeration getHeaderNames() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public String getHeader(String name) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public long getDateHeader(String name) {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-		
-		@Override
-		public Cookie[] getCookies() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public String getContextPath() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public String getAuthType() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-	};
-	HttpServletResponse resp = new HttpServletResponse() {
-		
-		@Override
-		public void setLocale(Locale loc) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public void setContentType(String type) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public void setContentLength(int len) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public void setCharacterEncoding(String charset) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public void setBufferSize(int size) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public void resetBuffer() {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public void reset() {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public boolean isCommitted() {
-			// TODO Auto-generated method stub
-			return false;
-		}
-		
-		@Override
-		public PrintWriter getWriter() throws IOException {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public ServletOutputStream getOutputStream() throws IOException {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public Locale getLocale() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public String getContentType() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public String getCharacterEncoding() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public int getBufferSize() {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-		
-		@Override
-		public void flushBuffer() throws IOException {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public void setStatus(int sc, String sm) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public void setStatus(int sc) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public void setIntHeader(String name, int value) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public void setHeader(String name, String value) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public void setDateHeader(String name, long date) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public void sendRedirect(String location) throws IOException {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public void sendError(int sc, String msg) throws IOException {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public void sendError(int sc) throws IOException {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public String encodeUrl(String url) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public String encodeURL(String url) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public String encodeRedirectUrl(String url) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public String encodeRedirectURL(String url) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public boolean containsHeader(String name) {
-			// TODO Auto-generated method stub
-			return false;
-		}
-		
-		@Override
-		public void addIntHeader(String name, int value) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public void addHeader(String name, String value) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public void addDateHeader(String name, long date) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public void addCookie(Cookie cookie) {
-			// TODO Auto-generated method stub
-			
-		}
-	};
-	HashMap<String, Object> maps;
-	
-	@Before
-	public void init(){
-		req.setAttribute("id", "jsy");
-		req.setAttribute("pw", "1234");
-	}
-	
-	
-	@Autowired
-	MainController mc;
-	
-	@Test
-	public void test() {
-		
-		System.out.println(req);
-		
-//		ModelAndView mav = mc.Login(this.mav, req, resp);
-		
-//		System.out.println(mav);
+	  @Autowired // WebContext Bean 받아오기
+	    private WebApplicationContext wac;
+	   
+	   // MockMVC 변수 생성
+	   private MockMvc mock;
+	   
+	   private int size = 7;
+	   private int tot = 7;
+	   private String start = "0";
+	   private String viewRow = "10";
+	   private String type = "FREE";
+	   private String URI = "/allData";
+	   
+	   @Before
+	   public void init(){ // MockMVC 에게 WebContext 정보 받아 오기 
+	      mock = MockMvcBuilders.webAppContextSetup(wac).build();
+	   }
+	      
+	   @Test
+	   public void BoardController() throws Exception {
+	      
+	      mock.perform(get(URI)           // get방식 : get("주소"), post방식 : post("주소") 
+	             .param("start", start) // paramater값 설정 : .param("key", "value")
+	                .param("viewRow", viewRow)
+	                .param("type", type))
+	      .andDo(new ResultHandler() {// 처리 내용을 출력합니다.
+	         @Override
+	         public void handle(MvcResult arg0) throws Exception {
+	            ModelAndView mav = arg0.getModelAndView();
+	            Map<String, Object> map = mav.getModel();
+	            System.out.println(map);
+	            
+	            String message = map.get("message").toString();
+	            System.out.println(message);
+	            JsonParser parser = new JsonParser();
+	            JsonElement element = parser.parse(message);
+
+	            JsonObject jobject = element.getAsJsonObject(); 
+	            JsonArray list = jobject.get("list").getAsJsonArray();
+	            System.out.println(list);
+	            assertEquals(size, list.size());
+	            
+	            JsonObject totCnt = jobject.get("totCntall").getAsJsonObject();
+	            assertEquals(tot, Integer.parseInt(totCnt.get("tot").toString()));
+	         }
+	      })
+	      .andExpect(status().isOk())// 상태값은 OK가 나와야 합니다.
+	      .andExpect(model().attributeExists("message"));// "message"이라는 attribute가 존재해야 합니다.
+	      
+
 	}
 	
 }
