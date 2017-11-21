b6598f3629b2d261a14712538342e0b7c2b213b4
GD <GD@DESKTOP-GO4SM0N>
2017-11-21 10:55:12 +0900

    notice까지

--- src/test/java/kr/gudi/teatime/TestCase.java
+++ src/test/java/kr/gudi/teatime/TestCase.java
@@ -1,11 +1,16 @@
 package kr.gudi.teatime;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.request;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.HashMap;
 import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,7 +22,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultHandler;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.google.gson.JsonElement;
@@ -29,13 +36,18 @@ import org.junit.Before;
 
 
 import kr.gudi.teatime.controller.MainController;
+import kr.gudi.teatime.dao.TestDaoInterface;
+import kr.gudi.util.HttpUtil;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"file:src/main/webapp/WEB-INF/spring/root-context.xml", "file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"})
 @WebAppConfiguration
+@SessionAttributes("id")
 public class TestCase {
 	
+	  @Autowired
+	  TestDaoInterface tdi;
 	
 	
 	  @Autowired // WebContext Bean 받아오기
@@ -50,16 +62,18 @@ public class TestCase {
 	   private String pw = "1234";
 	   private String URI = "/Login";
 	   
+	   
 	   @Before
 	   public void init(){ // MockMVC 에게 WebContext 정보 받아 오기 
 	      mock = MockMvcBuilders.webAppContextSetup(wac).build();
+	      
 	   }
 	      
 	   @Test
 	   public void BoardController() throws Exception {
 	      mock.perform(get(URI)           // get방식 : get("주소"), post방식 : post("주소") 
-	             .param("pw", "123") // paramater값 설정 : .param("key", "value")
-	             .param("id", "hi")
+	             .param("pw", "1234") // paramater값 설정 : .param("key", "value")
+	             .param("id", "jsy")
 	             )
 	      .andDo(new ResultHandler() {// 처리 내용을 출력합니다.
 	         @Override
@@ -86,8 +100,35 @@ public class TestCase {
 	      })
 	      .andExpect(status().isOk())// 상태값은 OK가 나와야 합니다.
 	      .andExpect(model().attributeExists("id"));// "id"이라는 attribute가 존재해야 합니다.
-	      
-
 	}
+	
+	   
+	   @Test
+	   public void notice() throws Exception {
+	      mock.perform(get("/notice")           // get방식 : get("주소"), post방식 : post("주소") 
+	             .param("pw", "123") // paramater값 설정 : .param("key", "value")
+	             .param("id", "jsy")
+	             )
+	      .andDo(new ResultHandler() {// 처리 내용을 출력합니다.
+	         @Override
+	         public void handle(MvcResult arg0) throws Exception {
+	            ModelAndView mav = arg0.getModelAndView();
+	            
+	           
+	            
+	            HttpServletRequest request = arg0.getRequest();
+	            HashMap<String, Object> param = HttpUtil.getParameterMap(request);
+	            System.out.println(param);
+	            
+	            System.out.println(mav.getViewName());
+	            
+	            assertEquals(tdi.loginCheck(param), param);
+	         }
+	      })
+	      .andExpect(status().isOk())// 상태값은 OK가 나와야 합니다.
+	      .andExpect(model().attributeExists("notice"));// "mapname"이라는 attribute가 존재해야 합니다.
+	}
+	   
+	   
 	
 }
